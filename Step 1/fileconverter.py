c302ce51944de654ff1ff799a67585acf13090e3
Sarah Dunn <sd2@hw.ac.uk>
2017-11-07 14:48:05 +0000

    Update fileconverter.py

--- Step 1/fileconverter.py
+++ Step 1/fileconverter.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
-###############
-# CSV to ARFF #
-###############
+#Data Mining and Machine Learning Coursework 2
+#Due Tuesday 7/11
+#Step 1
+#This file converts csv files to arff files
+
+#imports
 import csv
 import sys
 from time import sleep
@@ -10,18 +13,26 @@ from time import sleep
 #main function
 def main(argv):
 	
+	#store the filename temporarily
+	#this is so that fer2017.csv will not lose it's name
+	#it will become fer2017.arff
 	filename = sys.argv[1]
 	newfilename = filename.split(".")
 	
+	#writes new filename
 	o = open( newfilename[0] + ".arff",'w')
-	nmax = 0;
 	
+	#starts writing the file
+	#write relation line
 	o.write( "@relation " + newfilename[0] + "\n" )
+	#writes attribute line
 	o.write( "@ATTRIBUTE 'emotion' {'Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'}\n" )
 	
+	#creates a dictionary to convert the numbers to the words that we want 
+	#for our class attribute
 	dict = {'0' : "'Angry'", '1' : "'Disgust'", '2' : "'Fear'", '3' : "'Happy'", '4' : "'Sad'", '5' : "'Surprise'", '6' : "'Neutral'"}
 	
-	
+	#writes the instances in the file
 	for x in range(0, 2303):
 		o.write( "@attribute pixel%d NUMERIC\n" % (x) )
 		
@@ -36,6 +47,7 @@ def main(argv):
 				print( "Error, file has more data in one init" )
 				return
 				
+				#uses dictionary to replace numbers with words
 			o.write( dict[firstSplit[0]] + "," + firstSplit[1].replace( " ", "," ) )
 			
 	o.close()
